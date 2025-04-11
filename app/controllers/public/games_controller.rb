19f857598946d42628a4cf518ade0560044b76b3
kuropAqn <kuropan0819@gmail.com>
2025-04-11 10:57:08 +0000

    first commit

--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -1,7 +1,14 @@
 class Public::GamesController < ApplicationController
   def index
+    @games = Game.all
+    @genres = Genre.all
   end
 
   def show
+    @game = Game.find(params[:id])
+    @review = Review.new
+    @reviews = @game.reviews.includes(:user)
+    @average_score = @game.reviews.average(:score).to_f
   end
+
 end
