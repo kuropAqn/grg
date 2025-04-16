ec670d99f0bba0e03d06b47add2922a7f36c9f25
kuropAqn <kuropan0819@gmail.com>
2025-04-16 10:38:27 +0000

    fix application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,6 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import "jquery"
-import "popper.js"
-import "bootstrap"
-import "../stylesheets/application" 
 
 Rails.start()
 Turbolinks.start()
