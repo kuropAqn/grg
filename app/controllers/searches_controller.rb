92553f96e9191fc8c0dd80c9c53ca5ff842656cb
kuropAqn <kuropan0819@gmail.com>
2025-04-22 06:48:23 +0000

    fix header

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,22 @@
 class SearchesController < ApplicationController
 
-  def genre_search
-    @genre_id = params[:genre_id]
-    @games = Game.where(genre_id: @genre_id)
-  end
+  def search
+    @query = params[:query]
+    @search_type = params[:search_type]
+    @match_type = params[:match_type]
 
+    if @search_type == 'User'
+      @results = User.search_by_name(@query, @match_type)
+    elsif @search_type == 'Game'
+      @results = Game.search_by_title(@query, @match_type)
+      @pub_results = Game.search_by_title(@query, @match_type)
+    elsif @search_type == 'Genre'
+      @results = Genre.search_by_genre(@query, @match_type)
+      @pub_results = Genre.search_by_genre(@query, @match_type)
+    else
+      @results = []
+    end
+
+    render 'searches/search_results'
+  end
 end
