b9d05b41ce9b77ca30ecfd379b0d6b6ffa85ba37
kuropAqn <kuropan0819@gmail.com>
2025-04-24 12:13:56 +0000

    add star

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,10 +10,6 @@ import "channels"
 import "../stylesheets/application.scss"
 import "jquery"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
 import Raty from "raty.js"
 window.raty = function(elem,opt) {
   let raty =  new Raty(elem,opt)
@@ -21,3 +17,6 @@ window.raty = function(elem,opt) {
   return raty;
 }
 
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
