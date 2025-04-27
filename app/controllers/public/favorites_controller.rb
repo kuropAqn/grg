56589b68d14cce0d478deca0896fe808fa24b298
kuropAqn <kuropan0819@gmail.com>
2025-04-27 07:53:14 +0000

    favorite_btn ajax

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,21 @@
 class Public::FavoritesController < ApplicationController
   before_action :authenticate_user!
   def create
-    review = Review.find(params[:review_id])
-    favorite = current_user.favorites.new(review_id: review.id)
+    @review = Review.find(params[:review_id])
+    favorite = current_user.favorites.new(review_id: @review.id)
     favorite.save
-    # redirect_to request.referer
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
-    review = Review.find(params[:review_id])
-    favorite = current_user.favorites.find_by(review_id: review.id)
+    @review = Review.find(params[:review_id])
+    favorite = current_user.favorites.find_by(review_id: @review.id)
     favorite.destroy
-    # redirect_to request.referer
+    respond_to do |format|
+      format.js
+    end
   end
 
   private
